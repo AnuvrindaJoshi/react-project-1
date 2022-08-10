64bb8afb695f1829d41748af8e5079dc9a13f57b
anuvrinda joshi <dr.joshi1999@gmail.com>
2022-08-10 18:07:10 +0530

    login and signup page ui design done

--- src/Login.js
+++ src/Login.js
@@ -1,14 +1,29 @@
 import {Link} from 'react-router-dom';
+import log from './Images/login.png';
 function Login() {
   return (
-   
-    <div>
-        <h4><span>Digi</span>ency</h4>
-        <input placeholder='name'></input>
-        <input placeholder='mail'></input>
-        <p><Link to="/">Back to HomePage</Link></p>
-        <p>Dont have an account?<Link to='/Signup'>Sign up</Link></p>
-    </div>
+   <>
+   <div className="login">
+
+    <div className="login-content">
+    <h2><span>Digi</span>ency</h2>
+        <h1>Login to your Account</h1>
+        <form>
+        <input placeholder='Email'/>
+        <input placeholder='Password'/>
+        <button>Login</button>
+        </form>
+
+        <h1>New here?</h1>
+         <p> <Link to='/Signup'>Sign up</Link></p>
+     </div>
+
+     <div >
+        <img src={log} />
+     </div>
+
+     </div>
+    </>
   )
 }
 
--- src/SignUp.js
+++ src/SignUp.js
@@ -1,11 +1,29 @@
 import { Link } from "react-router-dom";
+import sign from './Images/signup.png';
+
 function SignUp(){
     return(
         <>
-        <h4><span>Digi</span>ency</h4>
-        <input placeholder='name'/>
-        <input placeholder='mail'/>
+        <div className="signup">
+            
+
+        <div>
+         <h2><span>Digi</span>ency</h2>
+        <img src={sign} />
+        </div>
+
+        <div className="signup-content">
+        <h1>Get <br/>
+        Started.</h1>
         <p>Already have an account?<Link to='/login'>Login</Link></p>
+        <form>
+        <input placeholder='Email'/>
+        <input placeholder='Password'/>
+        <button>Register</button>
+        </form>
+        </div>
+
+        </div>
         </>
     )
 }
